adae16719e514560c1e93637ee1cb27655110c6e
johannabergqvist <johannabergqvist@Johannas-MacBook-Pro.local>
2016-09-09 13:09:57 +0200

    lägga till alla filer för heruko

--- quizsite/settings.py
+++ quizsite/settings.py
@@ -1,3 +1,4 @@
+STATIC_ROOT = 'staticfiles'
 """
 Django settings for quizsite project.
 
@@ -75,11 +76,12 @@ WSGI_APPLICATION = 'quizsite.wsgi.application'
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': dj_database_url.config(
+        default='sqlite:////{0}'.format(os.path.join(BASE_DIR,
+'db.sqlite3'))
+    )
 }
+
 
 
 # Password validation
